7792795be2d48264a0934b229f46600aeef4574f
Sala K 401 <salak401@salasfr.itm.edu.co>
2026-05-26 20:42:34 -0500

    complemetar la hu1 porque manrique lo pidio

--- Principal.java
+++ Principal.java
@@ -3,5 +3,30 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Metodos m = new Metodos();
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("Bienvenidos a la practica de estructuras");
+            System.out.println("que desea realizar 1: , 2: 3: 4: salir");
+            int menu = sc.nextInt();
+            switch (menu) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+                    System.out.println("suerte que digo");
+                    continuar = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 }
